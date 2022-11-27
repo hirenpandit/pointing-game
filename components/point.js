4147a4edc4c7543f34c1e29ad747eaa13f82dd2a
hiren pandit <hirenppandit@gmail.com>
2022-11-27 23:06:37 +0530

    Added fun icons

--- components/point.js
+++ components/point.js
@@ -1,6 +1,8 @@
 import {useState, useEffect} from 'react'
 import { io } from 'socket.io-client'
 import styles from '../styles/Home.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle, faCommentDots } from '@fortawesome/free-regular-svg-icons'
 
 
 export default function Point({player, pts, show, id}){
@@ -35,6 +37,12 @@ export default function Point({player, pts, show, id}){
     return(
         <>
             <div className={styles.grid}>
+                <div className={styles.checkIcon}>
+                    {point > 0 
+                        ? <FontAwesomeIcon icon={faCheckCircle} /> 
+                        : <FontAwesomeIcon icon={faCommentDots} />
+                    }
+                </div>
                 <div className={styles.name}>{player}</div>
                 {show && 
                     <button className={styles.pointbtn}>{point}</button>
