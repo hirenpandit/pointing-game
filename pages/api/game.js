d9dc5e4b0a6e3d1f64dc344414993acf2457fc4e
hiren pandit <hirenppandit@gmail.com>
2022-12-03 21:26:26 +0530

    refactored api calls

--- pages/api/game.js
+++ pages/api/game.js
@@ -1,7 +1,6 @@
 import clientPromise from "../../lib/mongodb"
 
 export default async function game(req, res){
-    console.table(req.method)
     const client = await clientPromise
     const db = client.db('planning_poker')
 
