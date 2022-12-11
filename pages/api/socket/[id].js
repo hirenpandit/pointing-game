96d33e310ddaac06268e19fe3e263dcacf00b2fe
hiren pandit <hirenppandit@gmail.com>
2022-12-11 19:37:23 +0530

    added sharp

--- pages/api/socket/[id].js
+++ pages/api/socket/[id].js
@@ -17,6 +17,7 @@ export default async function handler(req, res) {
                 console.log(`joinded: ${data.id}`)
             })
             socket.on('point-update', data => {
+                console.log(`updating points ${data}`)
                 socket.to(data.id).emit('update-point', data)
             })
             
