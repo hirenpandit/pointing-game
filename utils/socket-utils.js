e80b9a9d1492afbc2edf3fde88971333577c3b12
hiren pandit <hirenppandit@gmail.com>
2022-12-18 21:41:40 +0530

    added socket endpoint for prod env

--- utils/socket-utils.js
+++ utils/socket-utils.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"
-const socket = io('https://pointing-game.vercel.app/')
+const socket = io('https://pointing-game.vercel.app/socket-io/')
 
 socket.on('connect-error', (err) => {
     console.log(err)
