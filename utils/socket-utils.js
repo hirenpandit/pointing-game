f94998b20dd8ed7869a2ae8f55075204423c3b80
hiren pandit <hirenppandit@gmail.com>
2022-12-26 19:01:30 +0530

    ec2 urll

--- utils/socket-utils.js
+++ utils/socket-utils.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"
-const socket = io('https://pointing-game.vercel.app/', {
+const socket = io('http://44.202.164.56', {
     transports: ['websocket'],
     reconnection: true
 })
