4b8328892b26ecaf5763647fb1c71dc7f2844038
hiren pandit <hirenppandit@gmail.com>
2022-12-29 22:38:11 +0530

    adjusted spinner

--- pages/loading.js
+++ pages/loading.js
@@ -1,6 +1,7 @@
 export default function Loading(){
     return(
-        <div class="spinner-border" role="status">
-      </div>
+        <div className="position-absolute top-50 start-50">
+          <div class="spinner-border" role="status"></div>
+        </div>
     )
 }
