e1718027cac341de8531cd299e342897702878c7
hiren pandit <hirenppandit@gmail.com>
2022-12-31 14:52:48 +0530

    close toast message

--- components/toast.js
+++ components/toast.js
@@ -1,24 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Toast from 'react-bootstrap/Toast';
 import ToastContainer from 'react-bootstrap/ToastContainer';
-
 import { useSelector } from 'react-redux'
 
 
 const ToastMsg = () => {
-
     const messages = useSelector(state=>state.messages.messages)
+    const [show, setShow] = useState([]);
     useEffect(() => {
-        console.log(`messages in store`)
-        console.log(messages)
+        const arr = Array.from(messages, msg => true)
+        setShow(arr)
     }, [messages])
+
+    const setShowStatus = (index) => {
+        setShow((prevState) => {
+            return prevState.map((prev, idx) => {
+                if(idx===index){
+                    prev = false;
+                }
+                return prev;
+            })
+        })
+    }
 
     return(
         <>
             <ToastContainer position='bottom-end' className='m-2'>
-                {messages.map(message => {
-                    return (<Toast key='1'>
+                {messages.map((message, index) => {
+                    return (<Toast key={index} onClose={() => setShowStatus(index)} show={show[index]}>
                         <Toast.Header>
                             <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
                             <strong className="me-auto">Message</strong>
